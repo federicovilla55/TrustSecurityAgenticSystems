be11b2a2699194b005cf029bd9b64b851b6be5ce
Federico Villa <federico.villa@epfl.ch>
2025-05-26 13:19:01 +0200

    Improved main Documentation.

--- src/main.py
+++ src/main.py
@@ -9,14 +9,14 @@ from src.runtime import Runtime, get_model, register_my_agent, register_orchestr
 async def run_setup():
     """
     The function is called to run the main application's backend using `uvicorn`, a python web framework
-    used to handle web connections from the browser or api client and allows
-    FastAPI to serve the actual request.
+    used to handle api connections. The main is called to initialize FastAPI and allowing it to serve the actual request.
+
     :return: None
     """
     try:
         uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
     except (RuntimeError, JWTError) as e:
-        print(f"FOUND ERROR: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     asyncio.run(run_setup())
